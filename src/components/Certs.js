aee86998bdd964f92d3ba1e5493039517eb480c2
Manish Singhal <agrawalmanish727@gmail.com>
2023-05-11 16:35:51 +0530

    Resume update

---
+++ src/components/Certs.js
@@ -0,0 +1,20 @@
+
+
+import hr from "../assets/curve-hr.svg"
+
+
+// import 
+
+export default function Certs(){
+    return (
+        <div id="certs" className="mt-4 text-white">
+            <h1 className="text-2xl font-bold">Certifications</h1>
+            <p className="font-light text-gray-400">Here are some of my Certifications</p>
+
+            {/* <div className="flex flex-col md:flex-row flex-wrap mt-4 gap-5"> */}
+            <div className="grid grid-cols-1 md:grid-cols-3 justify-center mt-4 gap-5">
+            </div>
+            <img src={hr} className="w-full mt-8 md:h-2" alt="hr" />
+        </div>
+    )
+}
